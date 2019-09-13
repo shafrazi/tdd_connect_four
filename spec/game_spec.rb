6d3848b8373a8a4fb3d78fac10e0ca80c5d78583
shafrazi <shafrazi@live.com>
2019-09-14 00:08:22 +0530

    add test to check game draw status

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -71,5 +71,49 @@ describe Game do
       cells[3][3].owner = "Player 2"
       expect(game3.check_for_win(cells[3][3])).to eql(true)
     end
+
+    it "returns false if all cells are occupied and no clear winner" do
+      game4 = Game.new
+      board = game.board
+      player1 = game.players[0]
+      player2 = game.players[1]
+      row0 = board.cells[0]
+      row1 = board.cells[1]
+      row2 = board.cells[2]
+      row3 = board.cells[3]
+      row4 = board.cells[4]
+      row5 = board.cells[5]
+
+      def board_filler(starter, player1, player2, row)
+        if starter == player1
+          row.each_with_index do |cell, i|
+            if i.even?
+              cell.owner = player1
+            else
+              cell.owner = player2
+            end
+            cell.occupied = true
+          end
+        else
+          row.each_with_index do |cell, i|
+            if i.even?
+              cell.owner = player2
+            else
+              cell.owner = player1
+            end
+            cell.occupied = true
+          end
+        end
+      end
+
+      board_filler(player1, player1, player2, row0)
+      board_filler(player2, player1, player2, row1)
+      board_filler(player2, player1, player2, row2)
+      board_filler(player1, player1, player2, row3)
+      board_filler(player2, player1, player2, row4)
+      board_filler(player2, player1, player2, row5)
+
+      expect(game4.check_for_win(row5[6])).to eql(false)
+    end
   end
 end
